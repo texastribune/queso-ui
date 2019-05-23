23d6ada1195b2c999eddc951d10a53494357b8e7
ashley-hebler <ashleyhebler@gmail.com>
2019-05-23 17:24:54 -0500

    Allow for array of icons instead of just dir

--- tasks/icons.js
+++ tasks/icons.js
@@ -40,8 +40,14 @@ const addSprite = async (currentSVG, spriteInstance) => {
 };
 
 const processSVGs = async dirMap => {
-  // find all .svg files in specified icon directory
-  const svgs = await glob(`${dirMap.in}*.svg`);
+  const input = dirMap.in;
+
+  let svgs = input;
+
+  // check if a whole directory is passed
+  if (typeof input === 'string') {
+    svgs = await glob(`${dirMap.in}*.svg`);
+  }
 
   // create a new svgstore instance
   const sprites = svgstore();
