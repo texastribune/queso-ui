0e343ed3060c43d8cc789621e0b1537323b5ffee
ashley-hebler <ashleyhebler@gmail.com>
2019-06-04 11:21:03 -0500

    Make js slightly less ugly

--- docs/src/js/ds.js
+++ docs/src/js/ds.js
@@ -5,34 +5,37 @@ showLegacy.addEventListener('click', function() {
   this.classList.toggle('is-active');
 });
 
+var baseFontSize = parseFloat(
+  window.getComputedStyle(document.documentElement).fontSize
+);
+
+function getSizes(el, prop) {
+  var compStyles = window.getComputedStyle(el);
+  var px = compStyles.getPropertyValue(prop);
+  var rems = parseFloat(px) / baseFontSize;
+  rems = parseFloat(rems.toFixed(3));
+  rems = rems.toString();
+  return `${prop}: ${rems}rem | ${px}`;
+}
+
 // inject t-size-<size> font-size
 var textBlocks = document.querySelectorAll('.text-size-demo');
-textBlocks.forEach(textBlock => {
-  var compStyles = window.getComputedStyle(textBlock);
-  var calcSize = compStyles.getPropertyValue('font-size');
-  var fontSetting =
-    parseFloat(calcSize) /
-    parseFloat(window.getComputedStyle(document.documentElement).fontSize);
-  var parentTextBlock = textBlock.parentNode.parentNode;
-  if (typeof parentTextBlock !== 'undefined') {
-    var div = parentTextBlock.querySelector('p');
-    var remVal = parseFloat(fontSetting.toFixed(3));
-    remVal = remVal.toString();
-    div.textContent = `font-size: ${remVal}rem | ${calcSize}`;
-  }
+textBlocks.forEach(el => {
+  var parentEl = el.parentNode.parentElement;
+  var p = parentEl.querySelector('p');
+  p.textContent = getSizes(el, 'font-size');
 });
 
 // inject l-container-<size> max-width
 var containers = document.querySelectorAll('.l-container');
-containers.forEach(container => {
-  var compStyles = window.getComputedStyle(container);
-  var maxWidth = compStyles.getPropertyValue('max-width');
-  var maxWidthSetting =
-    parseFloat(maxWidth) /
-    parseFloat(window.getComputedStyle(document.documentElement).fontSize);
-  container.querySelector(
-    'span'
-  ).textContent = `~ max-width: ${maxWidthSetting.toPrecision(
-    3
-  )}rem | ${maxWidth}`;
+containers.forEach(el => {
+  el.querySelector('span').textContent = getSizes(el, 'max-width');
+});
+
+// inject has-<size>-btm-marg max-width
+var marginHelpers = document.querySelectorAll('.margin-demo');
+marginHelpers.forEach(el => {
+  var parentEl = el.parentNode.parentNode.parentElement;
+  var p = parentEl.querySelector('p');
+  p.textContent = getSizes(el, 'margin-bottom');
 });
