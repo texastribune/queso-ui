eae042b7541e2c16ba019cae50dc9b3e64f82d34
ashley-hebler <ashleyhebler@gmail.com>
2020-06-29 11:42:58 -0500

    Remove unneeded dependencies

--- docs/config/tasks/copy.js
+++ docs/config/tasks/copy.js
@@ -10,12 +10,8 @@ const fs = require('fs-extra');
 const ora = require('ora');
 
 const copyFiles = async dirMap => {
-  try {
-    await fs.copy(dirMap.in, dirMap.out);
-    return `${dirMap.in} => ${dirMap.out}`;
-  } catch (err) {
-    throw err;
-  }
+  await fs.copy(dirMap.in, dirMap.out);
+  return `${dirMap.in} => ${dirMap.out}`;
 };
 
 module.exports = async mappedCopies => {
